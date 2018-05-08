89672f339e0a57ce4b42e6a650861d792c3254c1
gsduong <nguyenduong.ict.hust@gmail.com>
2018-05-08 14:43:48 +0700

    Update for heroku deploy

--- config/database.php
+++ config/database.php
@@ -1,5 +1,19 @@
 <?php
+$host = env('DB_HOST');
+$database = env('DB_DATABASE');
+$username = env('DB_USERNAME');
+$password = env('DB_PASSWORD');
 
+
+if($databaseUrl = getenv('DATABASE_URL')) {
+
+    $url = parse_url($databaseUrl);
+
+    $host = $url['host'];
+    $username = $url['user'];
+    $password = $url['pass'];
+    $database = substr($url['path'], 1);
+}
 return [
 
     /*
@@ -56,11 +70,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
